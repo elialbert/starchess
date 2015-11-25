1a03a101f0b0d13e733278f45e5f9c21da394978
elialbert <elialbert@gmail.com>
2015-11-25 11:31:47 -0600

    got signout working

--- strollz/config/routes.rb
+++ strollz/config/routes.rb
@@ -1,6 +1,11 @@
 Rails.application.routes.draw do
   devise_for :users, :controllers => { :omniauth_callbacks => "users/omniauth_callbacks" }
+  devise_scope :user do
+    get 'sign_out', :to => 'devise/sessions#destroy', :as => :destroy_user_session
+  end
+
   get 'welcome/index'
+  # get 'sign_out' => 'welcome#index'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
