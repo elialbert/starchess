26a76a0ab8aba641cadecd240eac87660e863561
Eli Albert <eli@provi.com>
2022-12-16 10:56:55 -0600

    deploy script

--- starchess/deploy.sh
+++ starchess/deploy.sh
@@ -1,3 +1,4 @@
 rsync -av --force --delete --progress --exclude-from=rsync_exclude.txt . eli@159.223.172.166:/home/eli/starchess/starchess/
-RAILS_ENV=production bundle exec rake assets:precompile
-sudo /etc/init.d/unicorn_starchess restart
+# RAILS_ENV=production bundle exec rake assets:precompile
+# fix env vars
+# sudo service unicorn_starchess restart
